53262caa3a4cc9a14a8da391c4b9290686948aa4
Rintia Islam <rintiaislam110@gmail.com>
2023-11-26 19:55:49 +0600

    survey api get from mongodb

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
+// password: 9auB5FU8fBIo1D3G
+// DB_USER: surveyUser
 
-
-const uri = "mongodb+srv://<username>:<password>@cluster0.cj9n1qe.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cj9n1qe.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,6 +27,17 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const surveysCollection = client.db('SurveyDB').collection('surveys');
+
+
+    // surveys api
+    app.get('/surveys', async (req, res) => {
+        const cursor = surveysCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+      })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");

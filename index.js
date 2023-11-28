4238c0b34a66f88b1187cc535a3a0ad885736347
Rintia Islam <rintiaislam110@gmail.com>
2023-11-28 13:10:04 +0600

    survey patch operation implemented

--- index.js
+++ index.js
@@ -16,43 +16,82 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
-    const surveysCollection = client.db('SurveyDB').collection('surveys');
+        const surveysCollection = client.db('SurveyDB').collection('surveys');
 
 
-    // surveys api
-    app.get('/surveys', async (req, res) => {
-        const cursor = surveysCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
-      })
+        // surveys api
+        app.get('/surveys', async (req, res) => {
+            const cursor = surveysCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        app.get('/surveys/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await surveysCollection.findOne(query);
+            res.send(result);
+        })
+
+        app.patch('/surveys/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updatedSurvey = req.body;
+            console.log(updatedSurvey);
+            const updateDoc = {
+              $set: {
+                totalVoted: updatedSurvey.totalVoted,
+                yesVoted: updatedSurvey.yesVoted,
+                noVoted: updatedSurvey.noVoted,
+                likes: updatedSurvey.likes,
+                dislikes: updatedSurvey.dislikes,
+                voters: updatedSurvey.voters
+              },
+            };
+            const result = await surveysCollection.updateOne(filter, updateDoc);
+            res.send(result);
+          })
+
+
+            // users
+
+            app.post('/users', async (req, res) => {
+                const newUser = req.body;
+                console.log(newUser);
+                const result = await surveysCollection.insertOne(newUser);
+                res.send(result);
+              })
+          
+
+           
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
     res.send('Survey snap is running')
-  })
-  
-  app.listen(port, () => {
+})
+
+app.listen(port, () => {
     console.log(`Survey snap Server is running on port ${port}`)
-  })
+})

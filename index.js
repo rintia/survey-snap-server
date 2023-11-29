99271dc7cc4bc865ddccce2d0fbe13122acfab3c
Rintia Islam <rintiaislam110@gmail.com>
2023-11-30 01:01:42 +0600

    jwt token and token verify

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const moment = require('moment');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config()
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -31,6 +32,29 @@ async function run() {
 
     const surveysCollection = client.db('SurveyDB').collection('surveys');
     const usersCollection = client.db('SurveyDB').collection('users');
+
+    // jwt related api
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({ token });
+    })
+
+      // middlewares 
+      const verifyToken = (req, res, next) => {
+        console.log('inside verify token', req.headers.authorization);
+        if (!req.headers.authorization) {
+          return res.status(401).send({ message: 'unauthorized access' });
+        }
+        const token = req.headers.authorization.split(' ')[1];
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+          if (err) {
+            return res.status(401).send({ message: 'unauthorized access' })
+          }
+          req.decoded = decoded;
+          next();
+        })
+      }
 
 
     // surveys api
@@ -131,20 +155,20 @@ async function run() {
 
     // users
 
-    app.post('/users', async (req, res) => {
+    app.post('/users', verifyToken, async (req, res) => {
       const newUser = req.body;
       console.log(newUser);
       const result = await usersCollection.insertOne(newUser);
       res.send(result);
     })
-    app.get('/users', async (req, res) => {
+    app.get('/users', verifyToken, async (req, res) => {
       const cursor = usersCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     })
 
 
-    app.put('/users/:id', async (req, res) => {
+    app.put('/users/:id', verifyToken, async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) }
       const updatedUser = req.body;

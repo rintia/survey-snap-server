646597b2c39e280fc4ac1b57a784806df7b25063
Rintia Islam <rintiaislam110@gmail.com>
2023-11-29 20:37:34 +0600

    user feedback added to db

--- index.js
+++ index.js
@@ -17,132 +17,148 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
 
 async function run() {
-    try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
 
-        const surveysCollection = client.db('SurveyDB').collection('surveys');
-        const usersCollection = client.db('SurveyDB').collection('users');
+    const surveysCollection = client.db('SurveyDB').collection('surveys');
+    const usersCollection = client.db('SurveyDB').collection('users');
 
 
-        // surveys api
-        app.get('/surveys', async (req, res) => {
-            const cursor = surveysCollection.find();
-            const result = await cursor.toArray();
-            res.send(result);
-        })
+    // surveys api
+    app.get('/surveys', async (req, res) => {
+      const cursor = surveysCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
 
-        app.get('/surveys/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) }
-            const result = await surveysCollection.findOne(query);
-            res.send(result);
-        })
+    app.get('/surveys/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await surveysCollection.findOne(query);
+      res.send(result);
+    })
 
-        app.post('/surveys', async (req, res) => {
-            const newSurvey = req.body;
-            newSurvey.timestamp = new Date();
-            console.log(newSurvey);
-            const result = await surveysCollection.insertOne(newSurvey);
-            res.send(result);
-          })
+    app.post('/surveys', async (req, res) => {
+      const newSurvey = req.body;
+      newSurvey.timestamp = new Date();
+      console.log(newSurvey);
+      const result = await surveysCollection.insertOne(newSurvey);
+      res.send(result);
+    })
 
-          app.put('/surveys/:id', async (req, res) => {
-            const id = req.params.id;
-            const filter = { _id: new ObjectId(id) }
-            const options = { upsert: true };
-            const updatedSurvey = req.body;
-      
-            const survey = {
-              $set: {
-                title: updatedSurvey.title,
-                question: updatedSurvey.question,
-                description: updatedSurvey.description
-              }
+    app.put('/surveys/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const options = { upsert: true };
+      const updatedSurvey = req.body;
+
+      const survey = {
+        $set: {
+          title: updatedSurvey.title,
+          question: updatedSurvey.question,
+          description: updatedSurvey.description
+        }
+      }
+      const result = await surveysCollection.updateOne(filter, survey, options);
+      res.send(result);
+    })
+
+    app.patch('/surveys/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedSurvey = req.body;
+      let result;
+      console.log(updatedSurvey);
+
+      if ('userFeedback' in updatedSurvey) {
+        // Handle user feedback update
+        const userFeedbackDoc = {
+            $set: {
+                userFeedback: updatedSurvey.userFeedback
             }
-            const result = await surveysCollection.updateOne(filter, survey, options);
-            res.send(result);
-          })
-
-        app.patch('/surveys/:id', async (req, res) => {
-            const id = req.params.id;
-            const filter = { _id: new ObjectId(id) };
-            const updatedSurvey = req.body;
-            console.log(updatedSurvey);
-
-
-            const updateDoc = {
-              $set: {
-                totalVoted: updatedSurvey.totalVoted,
-                yesVoted: updatedSurvey.yesVoted,
-                noVoted: updatedSurvey.noVoted,
-                likes: updatedSurvey.likes,
-                dislikes: updatedSurvey.dislikes,
-                votersEmails: updatedSurvey.votersEmails,
-                voters: updatedSurvey.voters,
-              },
-            };
-            const result = await surveysCollection.updateOne(filter, updateDoc);
-            res.send(result);
-          })
-
-
-            // users
-
-            app.post('/users', async (req, res) => {
-                const newUser = req.body;
-                console.log(newUser);
-                const result = await usersCollection.insertOne(newUser);
-                res.send(result);
-              })
-              app.get('/users', async (req, res) => {
-                const cursor = usersCollection.find();
-                const result = await cursor.toArray();
-                res.send(result);
-            })
-
-
-            app.put('/users/:id', async (req, res) => {
-                const id = req.params.id;
-                const filter = { _id: new ObjectId(id) }
-                const updatedUser = req.body;
-          
-                const user = {
-                  $set: {
-                   role: updatedUser.role
-                  }
-                }
-                const result = await usersCollection.updateOne(filter, user);
-                res.send(result);
-              })
-
-            
-
-           
-
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        // await client.close();
+        };
+       const result= await surveysCollection.updateOne(filter, userFeedbackDoc);
     }
+
+    else{
+
+      const updateDoc = {
+        $set: {
+          totalVoted: updatedSurvey.totalVoted,
+          yesVoted: updatedSurvey.yesVoted,
+          noVoted: updatedSurvey.noVoted,
+          likes: updatedSurvey.likes,
+          dislikes: updatedSurvey.dislikes,
+          votersEmails: updatedSurvey.votersEmails,
+          voters: updatedSurvey.voters,
+        },
+      };
+      const result = await surveysCollection.updateOne(filter, updateDoc);
+      
+    }
+      res.send(result);
+    })
+
+   
+
+
+    // users
+
+    app.post('/users', async (req, res) => {
+      const newUser = req.body;
+      console.log(newUser);
+      const result = await usersCollection.insertOne(newUser);
+      res.send(result);
+    })
+    app.get('/users', async (req, res) => {
+      const cursor = usersCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+
+    app.put('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const updatedUser = req.body;
+
+      const user = {
+        $set: {
+          role: updatedUser.role
+        }
+      }
+      const result = await usersCollection.updateOne(filter, user);
+      res.send(result);
+    })
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
 }
 run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Survey snap is running')
+  res.send('Survey snap is running')
 })
 
 app.listen(port, () => {
-    console.log(`Survey snap Server is running on port ${port}`)
+  console.log(`Survey snap Server is running on port ${port}`)
 })
